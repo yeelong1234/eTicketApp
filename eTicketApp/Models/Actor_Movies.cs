af04d09808baa62fec1a08d373ec0cfff0c31187
yeelong1234 <yeelong1234@gmail.com>
2023-08-02 21:36:23 +0800

    Update Models

---
+++ eTicketApp/Models/Actor_Movies.cs
@@ -0,0 +1,11 @@
+﻿namespace eTicketApp.Models
+{
+    public class Actor_Movies
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        public int ActorId { get; set; }
+        public Actor Actor { get; set; }
+    }
+}
